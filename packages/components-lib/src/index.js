f2d4e85422c4e530c20ee665850280e998160429
Nipun Arora <aroranipun1@gmail.com>
2024-07-31 17:49:52 +0530

    updates

---
+++ packages/components-lib/src/index.js
@@ -0,0 +1,5 @@
+import {Button as btn} from "./Button"
+
+export {
+  btn
+}
--- packages/react-app/src/root.component.js
+++ packages/react-app/src/root.component.js
@@ -1,3 +1,13 @@
+import {btn} from "components-lib";
+
 export default function Root(props) {
-  return <section>{props.name} is mounted!</section>;
+  const onCLickHandler = (e) => {
+    console.log(e);
+  }
+  return (
+    <div>
+      <btn onClick={onCLickHandler}>Click Me!</btn>
+      <section>{props.name} is mounted!</section>
+    </div>
+  )
 }
